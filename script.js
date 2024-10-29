c3bde017d70c386567b9970611cb845bc9af6ca6
PalaashBatra <78481121+PalaashBatra@users.noreply.github.com>
2024-10-28 22:24:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,3 @@ themeToggle.addEventListener('click', () => {
     themeToggle.textContent = 
         document.body.classList.contains('dark-mode') ? 'Switch to Light Mode' : 'Switch to Dark Mode';
 });
-
-// Dropdown Menu Functionality
-const dropdownBtn = document.getElementById('dropdown-btn');
-const dropdownContent = document.getElementById('dropdown-content');
-
-dropdownBtn.addEventListener('click', () => {
-    dropdownContent.classList.toggle('show');
-});
-
-// Close the dropdown if the user clicks outside
-window.addEventListener('click', (event) => {
-    if (!event.target.matches('.dropbtn')) {
-        if (dropdownContent.classList.contains('show')) {
-            dropdownContent.classList.remove('show');
-        }
-    }
-});
